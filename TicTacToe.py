5117a702dd2aba74dfe5f47452500cd27b7f3f4d
jamesl33 <jamesl33@posteo.net>
2016-10-18 14:48:33 +0100

    Added the code to allow the buttons to function

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,6 +3,7 @@ import tkinter as tk
 class Game(object):
 	def __init__(self):
 		#turn counter to determine if player places X or O
+		global turn
 		turn = True
 	def draw_interface(self):
 		#create and draw the root window
@@ -10,7 +11,11 @@ class Game(object):
 		#root windows title
 		root.title("Tic-Tac-Toe")
 		#size of the root window
-		root.geometry("600x700")
+		root.geometry("550x800")
+		#create label to push buttons down the root window
+		root.config()
+		lbl = tk.Label(root, height="2")
+		lbl.pack()
 		#create the three frames for the buttons
 		frame1 = tk.Frame(root, height="50", width="50")
 		frame1.pack()
@@ -18,22 +23,35 @@ class Game(object):
 		frame2.pack()
 		frame3 = tk.Frame(root, height="50", width="50")
 		frame3.pack()
+
+		self.buttons = []
 		for i in range(1,10):
 			if i <= 3:
-				btn = tk.Button(frame1, height="10", width="15", text=" ", \
+				btn = tk.Button(frame1, height="10", width="16", text=" ", \
 				command=lambda n=i: self.btn_pressed(n))
 				btn.pack(side="left")
 			elif i > 3 and i <=6:
-				btn = tk.Button(frame2, height="10", width="15", text=" ", \
+				btn = tk.Button(frame2, height="10", width="16", text=" ", \
 				command=lambda n=i: self.btn_pressed(n))
 				btn.pack(side="left")
 			else:
-				btn = tk.Button(frame3, height="10", width="15", text=" ", \
+				btn = tk.Button(frame3, height="10", width="16", text=" ", \
 				command=lambda n=i: self.btn_pressed(n))
 				btn.pack(side="left")
 
+			self.buttons.append(btn)
+
 	def btn_pressed(self, n):
-		pass
+		global turn
+		if turn == True:			
+			self.buttons[n-1].config(text="X")
+			self.buttons[n-1].config(state="disabled")
+			turn = False
+		else:
+			self.buttons[n-1].config(text="O")
+			self.buttons[n-1].config(state="disabled")
+			turn = True
+
 
 
 game = Game()
