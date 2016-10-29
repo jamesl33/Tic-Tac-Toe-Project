0e24d549ee8232b655dbfe3a294687293247565e
James L33 <jamesl33@posteo.net>
2016-10-29 17:15:36 +0000

    Added functions for reset game and draw board

--- Tic-Tac-Toe-Pygame.py
+++ Tic-Tac-Toe-Pygame.py
@@ -6,7 +6,7 @@ class TicTacToe(object):
 		#store the font that will be used in the variable "font"
 		self.font = pygame.font.SysFont("monospace", 300)
 		#create the pygame root screen called "screen"
-		self.screen = pygame.display.set_mode((600,600))
+		self.screen = pygame.display.set_mode((600,1000))
 		#"gameState" stores the current state of the game in the format of a list
 		self.gameState = [1,2,3,4,5,6,7,8,9]
 		#define a few colours that we will use as variables
@@ -14,14 +14,31 @@ class TicTacToe(object):
 		self.black = [000,000,000]
 		#fill the background of the root screen with the color "white"
 		self.screen.fill(self.white)
-		#draw lines to build the TicTacToe grid
+		self.draw_grid()
+		#game mainloop which deals with the running of the game
+		self.main_loop()
+
+	def draw_grid(self):
 		pygame.draw.line(self.screen,(self.black),(200,0),(200,600), (5))
 		pygame.draw.line(self.screen,(self.black),(400,0),(400,600), (5))
 		pygame.draw.line(self.screen,(self.black),(0,200),(600,200), (5))
 		pygame.draw.line(self.screen,(self.black),(0,400),(600,400), (5))
-		#game mainloop which deals with the running of the game
-		self.main_loop()
-	
+		pygame.draw.line(self.screen,(self.black),(0,0),(0,600), (5))
+		pygame.draw.line(self.screen,(self.black),(0,0),(600,0), (5))
+		pygame.draw.line(self.screen,(self.black),(600,0),(600,600), (5))
+		pygame.draw.line(self.screen,(self.black),(0,600),(600,600), (5))
+		pygame.draw.line(self.screen,(self.black),(400,620),(400,720), (5))
+		pygame.draw.line(self.screen,(self.black),(400,720),(600,720), (5))
+		pygame.draw.line(self.screen,(self.black),(400,620),(600,620), (5))
+
+	def reset_game(self, x, y):
+		if x <= 600 and x > 400:
+			if y <= 800 and y > 620:
+				self.gameState = [1,2,3,4,5,6,7,8,9]
+				self.turn = True
+				self.screen.fill(self.white)
+				self.draw_grid()
+
 	"""Mainloop function which is a While loop that runs the whole time the game is running. This function controls finding the location of the mouse when clicked
 	     and where the counter should be placed"""
 	def main_loop(self):
@@ -36,6 +53,7 @@ class TicTacToe(object):
 					x,y = event.pos
 					#pass the "gameState" "x" and "x" arguments to grid function
 					self.placement_grid(x, y)
+					self.reset_game(x, y)
 				#if the event is a QUIT event stop running the game 
 				if event.type == pygame.QUIT:
 					running = False
@@ -67,13 +85,12 @@ class TicTacToe(object):
 			print(n, "Wins ")
 		elif self.gameState[0] == n and self.gameState[3] == n and self.gameState[6] == n:
 			print(n, "Wins ")
-		elif self.gameState[1] == n and self.gameState[4] == "6" and self.gameState[7] == n:
+		elif self.gameState[1] == n and self.gameState[4] == n and self.gameState[7] == n:
 			print(n, "Wins ")
 		elif self.gameState[2] == n and self.gameState[5] == n and self.gameState[8] == n:
 			print(n, "Wins ")
 		elif self.gameState[2] == n and self.gameState[4] == n and self.gameState[6] == n:
 			print(n, "Wins")
-
 
 	"""This functions takes the "x" "y" variables and returns where the counter should be placed on the grid. This function also updates the "gameState" list which
 	store the current state of the game """	
