112acb006c16a885e5e630b5edd0494ab141cf16
James Lee <jamesl33info@gmail.com>
2016-10-30 15:41:01 +0000

    Updated DocStrings. Minor changes

--- Tic-Tac-Toe-Pygame.py
+++ Tic-Tac-Toe-Pygame.py
@@ -11,7 +11,9 @@ class TicTacToe(object):
 		self.font = pygame.font.SysFont("monospace", 300)
 		self.draw_grid()
 		self.main_loop()
-
+	"""Mainloop that handles running the game.This function calls
+	the other function when needed.When this loop ends the game 
+	has ended."""
 	def main_loop(self):
 		running = True
 		while running:
@@ -23,7 +25,10 @@ class TicTacToe(object):
 					self.reset_game(x,y)
 				if event.type == pygame.QUIT:
 					running = False
-
+	"""draw grid function which is were each of the lines used to make the grid are
+	declared and drawn. Each line has a beggining and end point and width.
+	"pygame.draw.line("Display",(Color),(X,Y),(X,Y),(With Value))" This is an example 
+	of how the pygame.draw.function is used."""
 	def draw_grid(self):
 		pygame.draw.line(self.screen,(self.black),(200,0),(200,600), (5))
 		pygame.draw.line(self.screen,(self.black),(400,0),(400,600), (5))
@@ -33,18 +38,22 @@ class TicTacToe(object):
 		pygame.draw.line(self.screen,(self.black),(0,0),(600,0), (5))
 		pygame.draw.line(self.screen,(self.black),(600,0),(600,600), (5))
 		pygame.draw.line(self.screen,(self.black),(0,600),(600,600), (5))
-		pygame.draw.line(self.screen,(self.black),(400,620),(400,720), (1))
-		pygame.draw.line(self.screen,(self.black),(400,720),(600,720), (1))
+		pygame.draw.line(self.screen,(self.black),(400,620),(400,670), (1))
+		pygame.draw.line(self.screen,(self.black),(400,670),(600,670), (1))
 		pygame.draw.line(self.screen,(self.black),(400,620),(600,620), (1))
-
+	"""Reset game function. This function resets all of the values back to the same
+	values that the game has when it is first initialised. This function also redraws
+	the grid and buttons."""
 	def reset_game(self, x, y):
 		if x <= 600 and x > 400:
-			if y <= 800 and y > 620:
+			if y <= 670 and y > 620:
 				self.gameState = [1,2,3,4,5,6,7,8,9]
 				self.turn = True
 				self.screen.fill(self.white)
 				self.draw_grid()
-
+	"""This function checks the current "gameState" to see if there has been a winner. It does this
+	by checking each direction on the board depending on the argument "n" N is the either "O" or "X"
+	"""
 	def check_for_win(self, n):
 		if self.gameState[0] == n and self.gameState[1] == n and self.gameState[2] == n:
 			print(n, "Wins ")
@@ -62,7 +71,8 @@ class TicTacToe(object):
 			print(n, "Wins ")
 		elif self.gameState[2] == n and self.gameState[4] == n and self.gameState[6] == n:
 			print(n, "Wins")
-
+		"""Placement grid function determine where the "O" or "X" should be drawn on the screen
+		this is done by comparing the "X" and "Y" values"""
 	def placement_grid(self, x, y):
 		if x <= 200:
 			if y < 200:
@@ -85,7 +95,9 @@ class TicTacToe(object):
 				self.place_counter(425, 212, 6)
 			elif y >= 400 and y <= 600:
 				self.place_counter(425, 412, 9)
-
+	"""This function handles the blitting of the "X" or "O" on the board. This is done
+	by blitting the label to the grid at the "x" and "y" coords give by the arguments "x" and 
+	"y"."""
 	def place_counter(self, x, y, n):
 		if self.turn == True:
 			for value in self.gameState:
