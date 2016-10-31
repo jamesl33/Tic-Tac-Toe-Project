da8703d229287caa1d5d0e9847649ad9b209ccec
RasAlgheti <g.beato@hotmail.com>
2016-10-31 09:06:29 +0000

    Changed the visuals
    Gave the window a title, a title icon and changed how the board looks.

--- Tic-Tac-Toe-Pygame.py
+++ Tic-Tac-Toe-Pygame.py
@@ -1,127 +1,140 @@
-import pygame
+import pygame   
 class TicTacToe(object):
 	def __init__(self):
 		pygame.init()
+		title_icon = pygame.image.load("TitleIcon.png")
+		pygame.display.set_caption("Tic-Tac-Toe")
+		pygame.display.set_icon(title_icon)
 		self.turn = True
+		#store the font that will be used in the variable "font"
+		self.font = pygame.font.SysFont("monospace", 100)
+		#create the pygame root screen called "screen"
+		self.screen = pygame.display.set_mode((400, 550))
+		#"gameState" stores the current state of the game in the format of a list
+		self.gameState = [1,2,3,4,5,6,7,8,9]
+		#define a few colours that we will use as variables
 		self.white = [255,255,255]
 		self.black = [000,000,000]
-		self.gameState = [1,2,3,4,5,6,7,8,9]
-		self.screen = pygame.display.set_mode((600,1000))
+		#fill the background of the root screen with the color "white"
 		self.screen.fill(self.white)
-		self.font = pygame.font.SysFont("monospace", 300)
-		self.font2 = pygame.font.SysFont("monospace", 40)
 		self.draw_grid()
+		#game mainloop which deals with the running of the game
 		self.main_loop()
-	"""Mainloop that handles running the game.This function calls
-	the other function when needed.When this loop ends the game 
-	has ended."""
-	def main_loop(self):
-		running = True
-		while running:
-			pygame.display.update()
-			for event in pygame.event.get():
-				if event.type == pygame.MOUSEBUTTONUP:
-					x,y = event.pos
-					self.placement_grid(x,y)
-					self.reset_game(x,y)
-				if event.type == pygame.QUIT:
-					running = False
-	"""draw grid function which is were each of the lines used to make the grid are
-	declared and drawn. Each line has a beggining and end point and width.
-	"pygame.draw.line("Display",(Color),(X,Y),(X,Y),(With Value))" This is an example 
-	of how the pygame.draw.function is used."""
-	def draw_grid(self):
-		pygame.draw.line(self.screen,(self.black),(200,0),(200,600), (5))
-		pygame.draw.line(self.screen,(self.black),(400,0),(400,600), (5))
-		pygame.draw.line(self.screen,(self.black),(0,200),(600,200), (5))
-		pygame.draw.line(self.screen,(self.black),(0,400),(600,400), (5))
-		pygame.draw.line(self.screen,(self.black),(0,0),(0,600), (5))
-		pygame.draw.line(self.screen,(self.black),(0,0),(600,0), (5))
-		pygame.draw.line(self.screen,(self.black),(600,0),(600,600), (5))
-		pygame.draw.line(self.screen,(self.black),(0,600),(600,600), (5))
-		pygame.draw.line(self.screen,(self.black),(400,620),(400,670), (1))
-		pygame.draw.line(self.screen,(self.black),(400,670),(600,670), (1))
-		pygame.draw.line(self.screen,(self.black),(400,620),(600,620), (1))
 
-		label = self.font2.render(("Reset Game"), 1, self.black)
-		self.screen.blit(label,(420, 632))
-	"""Reset game function. This function resets all of the values back to the same
-	values that the game has when it is first initialised. This function also redraws
-	the grid and buttons."""
+	def draw_grid(self):
+		pygame.draw.line(self.screen,(self.black),(50,50),(350,50), (5))
+		pygame.draw.line(self.screen,(self.black),(50,150),(350,150), (5))
+		pygame.draw.line(self.screen,(self.black),(50,250),(350,250), (5))
+		pygame.draw.line(self.screen,(self.black),(50,350),(350,350), (5))
+		pygame.draw.line(self.screen,(self.black),(50,50),(50,350), (5))
+		pygame.draw.line(self.screen,(self.black),(150,50),(150,350), (5))
+		pygame.draw.line(self.screen,(self.black),(250,50),(250,350), (5))
+		pygame.draw.line(self.screen,(self.black),(350,50),(350,350), (5))
+		pygame.draw.line(self.screen,(self.black),(50,400),(50,500), (5))
+		pygame.draw.line(self.screen,(self.black),(50,400),(350,400), (5))
+		pygame.draw.line(self.screen,(self.black),(50,500),(350,500), (5))
+		pygame.draw.line(self.screen,(self.black),(350,400),(350,500), (5))
+
 	def reset_game(self, x, y):
-		if x <= 600 and x > 400:
-			if y <= 670 and y > 620:
+		if x <= 350 and x > 50:
+			if y <= 500 and y > 400:
 				self.gameState = [1,2,3,4,5,6,7,8,9]
 				self.turn = True
 				self.screen.fill(self.white)
 				self.draw_grid()
-	"""This function checks the current "gameState" to see if there has been a winner. It does this
-	by checking each direction on the board depending on the argument "n" N is the either "O" or "X"
-	"""
-	def check_for_win(self, n):
-		if self.gameState[0] == n and self.gameState[1] == n and self.gameState[2] == n:
-			self.win(n)
-		elif self.gameState[3] == n and self.gameState[4] == n and self.gameState[5] == n:
-			self.win(n)
-		elif self.gameState[6] == n and self.gameState[7] == n and self.gameState[8] == n:
-			self.win(n)
-		elif self.gameState[0] == n and self.gameState[4] == n and self.gameState[8] == n:
-			self.win(n)
-		elif self.gameState[0] == n and self.gameState[3] == n and self.gameState[6] == n:
-			self.win(n)
-		elif self.gameState[1] == n and self.gameState[4] == n and self.gameState[7] == n:
-			self.win(n)
-		elif self.gameState[2] == n and self.gameState[5] == n and self.gameState[8] == n:
-			self.win(n)
-		elif self.gameState[2] == n and self.gameState[4] == n and self.gameState[6] == n:
-			self.win(n)
-		"""Placement grid function determine where the "O" or "X" should be drawn on the screen
-		this is done by comparing the "X" and "Y" values"""
-	def placement_grid(self, x, y):
-		if x <= 200:
-			if y < 200:
-				self.place_counter(25, 12, 1)
-			elif y >= 200 and y < 400:
-				self.place_counter(25, 212, 4)
-			elif y >= 400 and y <= 600:
-				self.place_counter(25, 412, 7)
-		elif x > 200 and x <= 400:
-			if y < 200:
-				self.place_counter(225, 12, 2)
-			elif y >= 200 and y < 400:
-				self.place_counter(225, 212, 5)
-			elif y >= 400 and y <= 600:
-				self.place_counter(225, 412, 8)
-		elif x > 400 and x <= 600:
-			if y < 200:
-				self.place_counter(425, 12, 3)
-			elif y >= 200 and y < 400:
-				self.place_counter(425, 212, 6)
-			elif y >= 400 and y <= 600:
-				self.place_counter(425, 412, 9)
-	"""This function handles the blitting of the "X" or "O" on the board. This is done
-	by blitting the label to the grid at the "x" and "y" coords give by the arguments "x" and 
-	"y"."""
+
+	
+			
+
+	"""Mainloop function which is a While loop that runs the whole time the game is running. This function controls finding the location of the mouse when clicked
+	     and where the counter should be placed"""
+	def main_loop(self):
+		#start the mainloop depending on the if the variable "running" is True 
+		running = True
+		while running:
+			pygame.display.update()
+			#for loop for all the events given from pygame
+			for event in pygame.event.get():
+				#when the player clicks on the screen
+				if event.type == pygame.MOUSEBUTTONUP:
+					x,y = event.pos
+					#pass the "gameState" "x" and "x" arguments to grid function
+					self.placement_grid(x, y)
+					self.reset_game(x, y)
+				#if the event is a QUIT event stop running the game 
+				if event.type == pygame.QUIT:
+					running = False
+
 	def place_counter(self, x, y, n):
 		if self.turn == True:
+			label = self.font.render("X", 1, self.black)
+			self.screen.blit(label, (x, y))
 			for value in self.gameState:
-				if value == n and type(value) == int:
-					label = self.font.render("X", 1, self.black)
-					self.screen.blit(label, (x+2, y))
+				if value == n:
 					self.gameState[value-1] = "X"
-					self.check_for_win("X")
-					self.turn = not self.turn
+			self.check_for_win("X")
 		else:
+			label = self.font.render("O", 1, self.black)
+			self.screen.blit(label, (x-4, y))
 			for value in self.gameState:
-				if value == n and type(value) == int:
-					label = self.font.render("O", 1, self.black)
-					self.screen.blit(label, (x-6, y))
+				if value == n:
 					self.gameState[value-1] = "O"
-					self.check_for_win("O")
-					self.turn = not self.turn
-	def win(self, n):
-		label = self.font2.render((n + " Wins"), 1, self.black)
-		self.screen.blit(label,(5, 620))
-		self.gameState = ["1", "2", "3", "4", "5", "6", "7", "8", "9",]
+				self.check_for_win("O")
+
+	def check_for_win(self, n):
+		
+		if self.gameState[0] == n and self.gameState[1] == n and self.gameState[2] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[3] == n and self.gameState[4] == n and self.gameState[5] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[6] == n and self.gameState[7] == n and self.gameState[8] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[0] == n and self.gameState[4] == n and self.gameState[8] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[0] == n and self.gameState[3] == n and self.gameState[6] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[1] == n and self.gameState[4] == n and self.gameState[7] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[2] == n and self.gameState[5] == n and self.gameState[8] == n:
+			print(n, "Wins ")
+			
+		elif self.gameState[2] == n and self.gameState[4] == n and self.gameState[6] == n:
+			print(n, "Wins")
+
+	"""This functions takes the "x" "y" variables and returns where the counter should be placed on the grid. This function also updates the "gameState" list which
+	store the current state of the game """ 
+	def placement_grid(self, x, y):
+		if x <= 150:
+			if y < 150:
+				self.place_counter(70, 45, 1)
+			elif y >= 150 and y < 250:
+				self.place_counter(70, 145, 4)
+			elif y >= 250 and y <= 350:
+				self.place_counter(70, 245, 7)
+		elif x > 150 and x <= 250:
+			if y < 150:
+				self.place_counter(170, 45, 2)
+			elif y >= 150 and y < 250:
+				self.place_counter(170, 145, 5)
+			elif y >= 250 and y <= 350:
+				self.place_counter(170, 245, 8)
+		elif x > 250 and x <= 350:
+			if y < 150:
+				self.place_counter(270, 45, 3)
+			elif y >= 150 and y < 250:
+				self.place_counter(270, 145, 6)
+			elif y >= 250 and y <= 350:
+				self.place_counter(270, 245, 9)
+		#change bool value to opposite value to allow alternating between placing "X" and "O"
+		if self.turn == True:
+			self.turn = False
+		else:
+			self.turn = True
 
 game = TicTacToe()
