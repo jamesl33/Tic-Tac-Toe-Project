bcc52bdc6c0206a80c991627b660e2deac21f795
RasAlgheti <g.beato@hotmail.com>
2016-11-04 09:57:48 +0000

    Changed visual settings
    (also made it so it actually closes)

--- ai.py
+++ ai.py
@@ -4,6 +4,6 @@ class ai(object):
 		if game.ai_mode == "easy":
 			self.random_ai(game)
 	def random_ai(self, game):
-			game.randomX = random.randint(1, 600)
-			game.randomY = random.randint(1, 600)
+			game.randomX = random.randint(50, 350)
+			game.randomY = random.randint(50, 350)
 			game.placement_grid(game.randomX,game.randomY)
