34e21f4b45d9f033ba61df13fb3287b9e978bde4
James Lee <jamesl33info@gmail.com>
2016-11-08 16:46:50 +0000

    simplified ai and fixed it

--- ai.py
+++ ai.py
@@ -1,21 +1,8 @@
 import random
 class ai(object):
-	def __init__(self, game):
-		if game.ai_mode == "easy":
-			self.random_ai(game)
-	def random_ai(self, game):
-		randomX = random.randint(1,3)
-		randomY = random.randint(1,3)
-		randomX = randomX * 100
-		randomY = randomY * 100
-		for index in game.ai_turns:
-			if index == [randomX,randomY]:
-				game.ai_turns.remove(index)
-				game.placement_grid(randomX,randomY)
-
-
-
-
-
-
-	
+	# def __init__(self, game):
+	# 	if game.ai_mode == "easy":
+			# self.random_ai(game)
+	def random_ai(self):
+		num = random.randint(1, 9)
+		return num
