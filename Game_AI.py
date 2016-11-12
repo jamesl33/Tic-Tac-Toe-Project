beffae3ef2a4a655b89dfd820322352b65f30104
James Lee <jamesl33info@gmail.com>
2016-11-12 08:15:54 +0000

    fixed ai bug and method of choosing move

--- Game_AI.py
+++ Game_AI.py
@@ -1,7 +1,12 @@
+import random
 class Game_Ai(object):
     def __init__(self):
         pass
-    def random_ai(self):
-        pass
+    def random_ai(self, lst):
+        while True:
+            num = random.choice(lst)
+            if type(num) == int:
+                return num 
+                break
     def hardened_ai(self):
         pass
