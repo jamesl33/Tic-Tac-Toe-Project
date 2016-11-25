c2e60c434fd888164d1eda2e823e77661988090d
James Lee <jamesl33info@gmail.com>
2016-11-25 13:55:23 +0000

    working server client

--- Client.py
+++ Client.py
@@ -1,17 +1,100 @@
-import socket, pickle, time
+import socket, select, sys, time, pickle
+
+class AlreadyConnected(Exception):
+	pass
+
+class NotConnected(Exception):
+	pass
 
 class Client:
-	def __init__(self):
-		self.server = socket.socket()
-		self.server.connect(("127.0.0.1", 12345))
+	def __init__(self, host=None, port=12345):
+		if host == None:
+			host = "127.0.0.1"
+		self.__host = host
+		self.__port = port
 
-	def send_message(self, message):
-		data = pickle.dumps(message)
-		self.server.send(data)
+		self.client = None
+		self.connect()
 
-user = Client()
+	def connect(self):
+		if self.connected():
+			raise AlreadyConnected()
+		self.__sendBuffer = []
+		self.__recvBuffer = ""
 
-while True:
-	user.send_message("Hello")
-	time.sleep(1)
+		self.client = socket.socket()
+		self.client.connect((self.__host, self.__port))
 
+	def connected(self):
+		return self.client != None
+
+	def shutdown(self):
+		if not self.connected():
+			return
+		self.client.close()
+		self.client = None
+		self.__recvBuffer = ""
+
+	def send_message(self, msg):
+		msgbytes = pickle.dumps(msg)
+		self.__sendBuffer.append(msgbytes)
+
+	def poll(self):
+		if not self.connected():
+			raise NotConnected()
+
+		read, write, error = select.select( [self.client], [self.client], [self.client], 0 )
+
+		messages = []
+
+		if error != []:
+			pass
+		try:
+			if read != []:
+				msg = self.client.recv(1024)
+
+				if len(msg) == 0:
+					raise ConnectionResetError()
+				else:
+					self.__recvBuffer += pickle.loads(msg)
+				msgs = self.__recvBuffer.split("\n")
+				messages = msgs[-1]
+				self.__recvBuffer = msgs[-1]
+
+			if write != []:
+				while len(self.__sendBuffer) != 0:
+					msg = self.__sendBuffer[0]
+
+					self.client.send(msg)
+					self.__sendBuffer.pop(0)
+
+		except ConnectionResetError:
+			self.shutdown()
+
+		return messages
+
+if __name__ == "__main__":
+	try:
+		client = Client()
+
+	except ConnectionRefusedError:
+		print("Connection failed")
+		sys.exit(1)
+
+	try:
+		print("Client connected")
+
+		client.send_message([1,2,3,4,5,6,7,8,9])
+
+		while True:
+			messages = client.poll()
+
+			for message in messages:
+				print("Recieved \"{}\"".format(message))
+
+	except (KeyboardInterrupt, NotConnected) as e:
+		pass
+
+	finally:
+		print("Shutdown")
+		client.shutdown()
--- Server.py
+++ Server.py
@@ -1,46 +1,60 @@
-import socket, pickle, sys, time
+import socket, select, pickle, time
+
+def unpickle_message(msgbytes):
+	return pickle.loads(msgbytes)
+
+
+def pickle_message(message):
+	msgbytes = pickle.dumps(message)
+	return (msgbytes)
+
 
 class Server:
-	def __init__(self):
-		self.clientList = []
-		self.socketList = []
+	def __init__(self, port=12345):
+		self.current_connections = []
+		self.current_sockets = []
+		self.port = port
 
-		self.create_server("", 12345)
-		self.start_server()
-
-	def create_server(self, ip, port):
 		self.server = socket.socket()
-		self.server.bind((ip, port))
-		self.server.listen(1)
+		self.server.bind(("", port))
+		self.server.listen(2)
 
-	# def send_message(self, message, user=None):
-	# 	data = pickle.loads(message)
-	# 	if user == None:
-	# 		for client in self.socketList:
-	# 			self.server.send(data)
-	# 	elif client in self.socketList:
-	# 		pass
+	def shutdown(self):
+		for client in self.current_connections:
+			client.close()
 
-	def start_server(self):
-		while len(self.clientList) != 1:
-			print("waiting for one or more connections")
-			(csocket, caddress) = self.server.accept()
-			self.clientList.append(caddress)
-			self.socketList.append(csocket)
-		else:
-			print("Two people have now connected")
-			try:
-				while True:
-					for client in self.socketList:
-						data = client.recv(2048)
-						data = pickle.loads(data)
-						print(data)
-				time.sleep(2)
-			except EOFError:
-				pass
-
-	def close_server(self):
 		self.server.shutdown(1)
 		self.server.close()
 
-server = Server()
+	def poll(self):
+		read, write, error = select.select(self.current_connections+[self.server], self.current_connections, self.current_connections, 0)
+
+		for connection in read:
+			if connection is self.server:
+				client, address = connection.accept()
+				self.current_connections.append(client)
+
+				print("Got connection from {}".format(address))
+
+			else:
+				msgbytes = connection.recv(1024)
+				msgbytes = unpickle_message(msgbytes)
+				print(msgbytes)
+				if not msgbytes:
+					print("Connection disconnected")
+
+if __name__ == "__main__":
+	server = Server()
+
+	try:
+		print("Server is running on port {}".format(server.port))
+
+		while True:
+			server.poll()
+
+	except KeyboardInterrupt:
+		pass
+
+	finally:
+		print("Shutdown")
+		server.shutdown()
