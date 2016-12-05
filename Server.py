84e796900d962aa6169fe5ed6b97d3d5f4ba7410
RasAlgheti <g.beato@hotmail.com>
2016-12-05 10:50:11 +0000

    Added docstrings (pls look they're pretty shitty)

--- Server.py
+++ Server.py
@@ -1,102 +1,100 @@
+#This modules holds all the methods and data to run a server for the game.
 import socket, select, pickle, time, Game_Functions
 
 def unpickle_message(msgbytes):
-	return pickle.loads(msgbytes)
+    """Function in charge of unpickling data and outputting it."""
+        
+    return pickle.loads(msgbytes)
 
 
 def pickle_message(message):
-	msgbytes = pickle.dumps(message)
-	return (msgbytes)
+    """Function in charge of pickling data and outputting it."""
+    msgbytes = pickle.dumps(message)
+    return (msgbytes)
+
 
 class Server:
-	def __init__(self, port=12345):
-		self.current_connections = []
-		self.current_sockets = []
-		self.sendBuffer = ""
-		self.port = port
-		self.functions = Game_Functions.Functions([1,2,3,4,5,6,7,8,9])
-		self.server = socket.socket()
-		self.server.bind(("", port))
-		self.server.listen(2)
-		self.turn = True
+    def __init__(self, port=12345):
+        """This sets the default values for the variables that are to be used by the server."""
+                
+        self.current_connections = []
+        self.current_sockets = []
+        self.sendBuffer = ""
+        self.port = port
+        self.functions = Game_Functions.Functions([1,2,3,4,5,6,7,8,9])
+        self.server = socket.socket()
+        self.server.bind(("", port))
+        self.server.listen(2)
+        self.turn = True
 
-	def send_message(self, msg, client=None):
-		msgbytes = pickle.dumps(msg)
-		if client == None:
-			for client in self.current_connections:
-				client.send(msgbytes)
-		else:
-			print("Done know that client")
+    def shutdown(self):
+        """Function that closes all connections to any clients that are connected and then shuts down the server itself."""
+        for client in self.current_connections:
+            client.close()
 
-	def shutdown(self):
-		for client in self.current_connections:
-			client.close()
+        self.server.shutdown(1)
+        self.server.close()
 
-		self.server.shutdown(1)
-		self.server.close()
+    def take_turn(self, msg):
+        """Function that handles turn taking in multiplayer mode."""
+        print(msg)
+        pos = msg[1]
+        x = pos[0]
+        y = pos[1]
+        if x > 230 and x < 380:
+                if y > 370 and y < 415:
+                    print("Reset")
+        if msg[0] == True and self.turn == True:
+            msgbytes = pickle.dumps(["Draw", "X", self.functions.placement_grid(x,y)])
+            self.turn = not self.turn
+            for connection in self.current_connections:
+                connection.send(msgbytes)
+        elif msg[0] == False and self.turn == False:
+            msgbytes = pickle.dumps(["Draw", "O", self.functions.placement_grid(x,y)])
+            self.turn = not self.turn
+            for connection in self.current_connections:
+                connection.send(msgbytes)
 
-	def take_turn(self, msg):
-		pos = msg[1]
-		x = pos[0]
-		y = pos[1]
-		if x > 230 and x < 380:
-				if y > 370 and y < 415:
-					print("Reset")
-		if msg[0] == True and self.turn == True:
-			msgbytes = pickle.dumps(["Draw", "X", self.functions.placement_grid(x,y)])
-			self.turn = not self.turn
-			for connection in self.current_connections:
-				connection.send(msgbytes)
-		elif msg[0] == False and self.turn == False:
-			msgbytes = pickle.dumps(["Draw", "O", self.functions.placement_grid(x,y)])
-			self.turn = not self.turn
-			for connection in self.current_connections:
-				connection.send(msgbytes)
-		elif msg[0] == "Reset":
-			msgbytes = pickle.dumps(["Reset"])
-			for connection in self.current_connections:
-				connection.send(msgbytes)
+    def poll(self):
+        """Function that takes care of receiving and sending messages to and from the server"""
+        read, write, error = select.select(self.current_connections+[self.server], self.current_connections, self.current_connections, 0)
 
-	def poll(self):
-		read, write, error = select.select(self.current_connections+[self.server], self.current_connections, self.current_connections, 0)
+        for connection in read:
+            if connection is self.server:
+                client, address = connection.accept()
+                self.current_connections.append(client)
+                turn = True
+                if len(self.current_connections) == 2:
+                    for client in self.current_connections:
+                        msgbytes = pickle.dumps(turn)
+                        client.send(msgbytes)
+                        turn = not turn
 
-		for connection in read:
-			if connection is self.server:
-				client, address = connection.accept()
-				self.current_connections.append(client)
-				turn = True
-				if len(self.current_connections) == 2:
-					for client in self.current_connections:
-						msgbytes = pickle.dumps(turn)
-						client.send(msgbytes)
-						turn = not turn
+                print("Got connection from {}".format(address))
 
-				print("Got connection from {}".format(address))
+            else:
+                msgbytes = connection.recv(2048)
+                if len(msgbytes) != 0:
+                    msg = pickle.loads(msgbytes)
+                    self.take_turn(msg)
+                    if not msgbytes:
+                        print("Connection disconnected")
 
-			else:
-				msgbytes = connection.recv(2048)
-				if len(msgbytes) != 0:
-					msg = pickle.loads(msgbytes)
-					self.take_turn(msg)
-					if not msgbytes:
-						print("Connection disconnected")
-
-		for connection in write:
-			if write != []:
-				pass
+        for connection in write:
+            if write != []:
+                pass
 
 if __name__ == "__main__":
-	server = Server()
+    server = Server()
 
-	try:
-		print("Server is running on port {}".format(server.port))
-		server.send_message("brekfast")
-		while True:
-			server.poll()
+    try:
+        print("Server is running on port {}".format(server.port))
+        while True:
+            server.poll()
 
-	except KeyboardInterrupt:
-		pass
+    except KeyboardInterrupt:
+        pass
 
-	finally:
-		print("Shutdown")
-		server.shutdown()
+    finally:
+        print("Shutdown")
+        server.shutdown()
